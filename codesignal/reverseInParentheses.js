d465208e671bc5a4cb92680450e2b4b1efd520ab
saintograph <winfredselwyn@gmail.com>
2019-02-05 23:19:18 +0800

    Reverse in Parentheses attempt.

--- codesignal/reverseInParentheses.js
+++ codesignal/reverseInParentheses.js
@@ -43,3 +43,62 @@ reverseInParentheses('foo(bar(baz))blim') // "foobazrabblim"
 // /a\s?fruit/
 
 // .match(/\(([^)]+)\)/)[1]
+
+
+
+function reverseInParentheses(inputString) {
+  const s = inputString.split('').join('');
+  const s2 = s.replace(/[\W_]+/g," ").split('');
+  const s3 = s2.join('');
+  // const reverseString = s3.match(/\s[a-z ]+\s/).join('').split('');
+  let firstIndex = 0;
+  let lastIndex = 0;
+  for(let i = 0; i < s2.length; i++) {
+    if(s2[i] === ' ' && lastIndex === 0) {
+      firstIndex = i;
+    }
+    if(s2[i] === ' ') {
+      lastIndex = i;
+    }
+  }
+  return s2.splice(3, 11 - 2)
+}
+
+reverseInParentheses("foo(bar(baz))blim"); // "foobazrabblim"
+
+
+
+// 5/2/2019
+
+function reverseInParentheses(inputString) {
+  let s = inputString.split('');
+  let first = 0;
+  let last = 0;
+  let rs = [];
+  const reverseArray = (arr) => {
+    for(let i = 0; i < arr.length; i++) {
+      if(arr[i] === '(') {
+        first = i;
+      }
+      if(arr[i] === ')') {
+        last = i;
+        break;
+      }
+    }
+    console.log(arr)
+    rs = arr.splice(first, last - first + 1);
+    rs = rs.reverse().join('').replace(/[^\w\s]/gi, '')//.split('');
+  }
+  reverseArray(s)
+  s.splice(first, 0, ...rs);
+  s = s.join('').split('');
+  first = 0;
+  end = 0;
+  rs.length = 0;
+  if(s.includes('(')) {
+    reverseArray(s)
+    s.splice(first, 0, ...rs);
+    s = s.join('');
+  }
+  return Array.isArray(s) ? s.join('') : s;
+}
