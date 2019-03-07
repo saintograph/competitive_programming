01bf21466eae25041cf1bf0d452e536b10429b2a
saintograph <winfredselwyn@gmail.com>
2019-03-07 10:09:41 +0800

    Solved Box Blur.

---
+++ codesignal/boxBlur.js
@@ -0,0 +1,40 @@
+/* 
+  Last night you partied a little too hard. Now there's a black and white photo of you that's about to go viral! 
+  You can't let this ruin your reputation, so you want to apply the box blur algorithm to the photo to hide its content.
+  The pixels in the input image are represented as integers. 
+  The algorithm distorts the input image in the following way: Every pixel x in the output image has a value equal to the average 
+  value of the pixel values from the 3 × 3 square that has its center at x, including x itself. All the pixels on the border of x are then removed.
+  Return the blurred image as an integer, with the fractions rounded down.
+*/
+
+const add = (a, b) => a + b;
+const box = (a, i, j) => {
+  const res = [];
+  // top
+  res.push(a[i - 1][j - 1], a[i - 1][j], a[i - 1][j + 1])
+  // current
+  res.push(a[i][j - 1], a[i][j], a[i][j + 1])
+  // bottom
+  res.push(a[i + 1][j - 1], a[i + 1][j], a[i + 1][j + 1])
+  return Math.floor(res.reduce(add) / 9);
+}
+
+function boxBlur(im) {
+  let result = [];
+  for (let i = 1; i <= im.length - 2; i++) {
+    result.push([])
+    for (let j = 1; j <= im[i].length - 2; j++) {
+      result[i - 1].push(box(im, i, j));
+    }
+  }
+  return result;
+};
+
+
+const image = [
+  [36, 0, 18, 9],
+  [27, 54, 9, 0],
+  [81, 63, 72, 45]
+]
+
+boxBlur(image); // [[40, 30]]
--- codesignal/longestUncorruptedSegment.js
+++ codesignal/longestUncorruptedSegment.js
@@ -1,52 +1,52 @@
-function longestUncorruptedSegment(a, b) {
-  let start = 0;
-  let length = 0;
-  const uncorrupted = [];
-  function* generator(i) {
-    let index = 1;
-    while (index < index + 1) {
-      yield index++;
-    }
-  }
-  const gen = generator(0);
+// unoptimized
 
-  if(a.length === 1) {
+function longestUncorruptedSegment(s, d) {
+  let length = 0;
+  let res = [];
+  let temp = [];
+  if(s.length < 2) {
     return [0, 0];
   }
-  if(a[0] !== b[0]) {
-    return [0, 0]
-  }
-  let idx = 0;
-  for(let i = 0; i < a.length; i++) {
-    if(a[i] !== b[i]) {
-      idx = gen.next().value;
-    }
-    if(a[i] === b[i]) {
-      uncorrupted[idx] = Array.isArray(uncorrupted[idx])? uncorrupted[idx] : [];
-      uncorrupted[idx].push(a[i])
+  for(let i = 0; i < s.length; i++) {
+    if(s[i] !== d[i]) {
+      s[i] = ' ';
     }
   }
-  const longest = () => {
-    const clean = uncorrupted.filter(x => x !== null);
-    const longest = clean.map(function(a){return a.length;}).indexOf(Math.max.apply(Math, clean.map(function(a){return a.length;})))
-    console.log(clean)
-    console.log(clean[longest])
-    let long = 0;
-    for(let j = 0; j < clean.length; j ++) {
-      long = long < clean[j].length ? clean[j].length : long;
-    }
-    // return long;
-    return [long, a.indexOf(clean[longest][0])];
+  if(s === ' '.repeat(s.length)) {
+    return [0, 0];
   }
-  // [length, starting index]
-  // return [longest(), start];
-  return longest()
-}
+  for(let j = 0; j < s.length; j++) {
+    if(s[j] === ' ') {
+      if(temp.length > length) {
+        length = temp.length;
+        res = [];
+        res.push(...temp)
+        temp = [];
+      }
+    } else if(s.indexOf(s[j]) === s.length - 1) {
+      if(temp.length > length) {
+        if(s[j] !== ' ') {
+          temp.push(s[j])
+        }
+        length = temp.length;
+        res = [];
+        res.push(...temp)
+        temp = [];
+      }
+    } else {
+      temp.push(s[j]);
+    }
+  }
+  return [length, d.indexOf(res[0]) === -1 ? 0 : d.indexOf(res[0])]
+};
 
-// const a = [33531593, 96933415, 28506400, 39457872, 29684716, 86010806];
-// const b = [33531593, 96913415, 28506400, 39457872, 29684716, 86010806];
-const a = [92988800, 80253955, 17396563, 91682092, 77708269, 97587946, 23889892, 20661856, 21013095, 92028000, 17562863, 86804822, 17819093, 97941923, 64955308]; 
-const b = [92988800, 80253955, 17396563, 91682092, 77708229, 97587946, 23889892, 20661866, 21013095, 92928000, 17962863, 86804822, 14819093, 97241923, 62955308];
-// const a = [2, 3, 5, 7, 55, 1];
-// const b = [2, 3, 5, 6, 55, 0]
-longestUncorruptedSegment(a, b) // [4, 2]
+// const sourceArray = [33531593, 96933415, 28506400, 39457872, 29684716, 86010806]
+// const destinationArray = [33531593, 96913415, 28506400, 39457872, 29684716, 86010806]
+
+// const sourceArray = [99919628, 77504204, 18846830, 86785029, 86230362, 96953294, 53208680, 95327090, 68996760, 26366538, 90490275, 62583792, 87514087, 96921389, 21309822]
+// const destinationArray = [99919628, 77503204, 18546830, 86785029, 86230362, 96953264, 53208680, 95327090, 68996760, 26366538, 90420275, 62583792, 87514087, 39692139, 21303822]
+
+const sourceArray = [20800440, 98256958, 64277103, 40475664, 98589505, 31621824, 84322264, 58283379, 15631261, 35464021]
+const destinationArray = [20800440, 95256958, 64276103, 40475664, 98589505, 31621824, 84322264, 58283379, 15631261, 35464021] // [7, 3]
+
+longestUncorruptedSegment(sourceArray, destinationArray); // [4, 2]
